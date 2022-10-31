b1490c5b6fda1c790d7e0341a39b6ece4c4d3f55
TenViki <viktorkomarek28@gmail.com>
2022-10-31 17:47:34 +0100

    :sparkles: Adds dynamic background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import Hero from "./sections/hero/Hero";
 
 function App() {
   return (
     <div className="app">
       <Hero />
+      <div style={{ height: "100vh", backgroundColor: "#121245" }}>
+        another thing
+      </div>
     </div>
   );
 }
--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,14 +1,18 @@
-import React from "react";
 import HeroText from "./HeroText";
+import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <div className="hero-background">
+    <div className="hero">
       <div className="hero-text">
         <h1>Hey there! My name is Viki</h1>
         <h2>
           and I'm a <HeroText />
         </h2>
+      </div>
+      <div className="hero-background">
+        <div className="hero-background-1" />
+        <div className="hero-background-2" />
       </div>
     </div>
   );
