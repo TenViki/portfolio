e2831760a58e3f4bdb0da55e02bd2136a7a9724a
TenViki <viktorkomarek28@gmail.com>
2022-12-23 10:15:23 +0100

    :sparkles: Adds configuration for DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configServce: ConfigService) => ({
+        type: "postgres",
+        host: "localhost",
+        port: configServce.get("DB_PORT"),
+        username: configServce.get("DB_USERNAME"),
+        password: configServce.get("DB_PASSWORD"),
+        database: configServce.get("DB_NAME"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
